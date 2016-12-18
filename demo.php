8f58b57d72187ea24ea8f87d452507d05e19673e
Luciano E. Fontana <lfontana@dsyr.com.ar>
2016-12-18 18:57:11 -0300

    cambio menor
    cambio de nombre de files, y organizacion

--- demo.php
+++ demo.php
@@ -1,13 +1,15 @@
 <?php  
 error_reporting(E_ALL);
 
-include 'funciones.php';
+include 'api_sms_portech.php';
 
 EnviarSMS('1131242954','Testing metodos 1a','2954','1301','1');
 EnviarSMS('1131242954','Testing metodos 1b','2954','1301','2');
 EnviarSMS('1131242954','Testing metodos 2a','2954','1302','1');
 EnviarSMS('1131242954','Testing metodos 2b','2954','1302','2');
 EnviarSMS('1131242954','Testing metodos 2b','2954','1302','2');
+
+
 //Parametros de la funcion EnviarSMS:
 /*
 1) Celular destino
