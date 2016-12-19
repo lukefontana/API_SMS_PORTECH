f0009ac080de67ed5ec3d068b515f4f75193e0c3
Luciano E. Fontana <lfontana@dsyr.com.ar>
2016-12-18 21:55:21 -0300

    modificaciones leves
    Se cambio el numero de movil de pruebas

--- ASP/demo.php
+++ ASP/demo.php
@@ -3,11 +3,11 @@ error_reporting(E_ALL);
 
 include 'api/api_sms_portech.php';
 
-EnviarSMS('1131242954','Testing metodos 1a','2954','1301','1');
-EnviarSMS('1131242954','Testing metodos 1b','2954','1301','2');
-EnviarSMS('1131242954','Testing metodos 2a','2954','1302','1');
-EnviarSMS('1131242954','Testing metodos 2b','2954','1302','2');
-EnviarSMS('1131242954','Testing metodos 2b','2954','1302','2');
+EnviarSMS('1131241300','Testing metodos 1a','2954','1301','1');
+EnviarSMS('1131241321','Testing metodos 1b','2954','1301','2');
+EnviarSMS('1131212544','Testing metodos 2a','2954','1302','1');
+EnviarSMS('1131244455','Testing metodos 2b','2954','1302','2');
+EnviarSMS('1131245554','Testing metodos 2b','2954','1302','2');
 
 
 //Parametros de la funcion EnviarSMS:
